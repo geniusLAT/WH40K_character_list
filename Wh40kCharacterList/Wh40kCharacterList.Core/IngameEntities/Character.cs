5af3e05f14d616ca1596458f17ddd5d529c0088a
geniuslat <traber73@yandex.ru>
2026-09-22 15:14:12 +0400

    added tab

--- Wh40kCharacterList/Wh40kCharacterList.Core/IngameEntities/Character.cs
+++ Wh40kCharacterList/Wh40kCharacterList.Core/IngameEntities/Character.cs
@@ -2,5 +2,9 @@
 
 public class Character
 {
+    public string Name { get; set; } = string.Empty;
 
+    public string Description { get; set; }
+
+    
 }
--- Wh40kCharacterList/Wh40kCharacterList/CharacterGui/CharacterFormManager.cs
+++ Wh40kCharacterList/Wh40kCharacterList/CharacterGui/CharacterFormManager.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wh40kCharacterList.Core.IngameEntities;
 
 namespace Wh40kCharacterList.CharacterGui;
 
 internal class CharacterFormManager
 {
+    public Character _chosenCharacter;
+
+    public void RenderCharacter(Character character)
+    {
+
+    }
 }

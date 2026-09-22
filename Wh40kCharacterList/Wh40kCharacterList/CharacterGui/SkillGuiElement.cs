130d8339734a1f43a11452863ed15f87aef6cc66
geniuslat <traber73@yandex.ru>
2026-09-22 22:16:12 +0400

    added SkillGuiElement.cs

---
+++ Wh40kCharacterList/Wh40kCharacterList/CharacterGui/SkillGuiElement.cs
@@ -0,0 +1,12 @@
+﻿using Wh40kCharacterList.Core.IngameEntities.CharacterProperties;
+
+namespace Wh40kCharacterList.CharacterGui;
+
+internal class SkillGuiElement
+{
+    public required Panel Panel { get; set; }
+
+    public required Label Label { get; set; }
+
+    public required SkillLevel SkillLevel { get; set; }
+}

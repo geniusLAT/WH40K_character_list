aba11208007b0d22946822f420f4901d7490e830
geniuslat <traber73@yandex.ru>
2026-09-22 15:38:51 +0400

    Characteristic panel draft

--- Wh40kCharacterList/Wh40kCharacterList.Core/IngameEntities/Character.cs
+++ Wh40kCharacterList/Wh40kCharacterList.Core/IngameEntities/Character.cs
@@ -6,7 +6,7 @@ public class Character
 {
     public string Name { get; set; } = string.Empty;
 
-    public string Description { get; set; }
+    public string Description { get; set; } = string.Empty ;
 
     #region Characteristics
 
--- Wh40kCharacterList/Wh40kCharacterList/CharacterGui/CharacterForm.Designer.cs
+++ Wh40kCharacterList/Wh40kCharacterList/CharacterGui/CharacterForm.Designer.cs
@@ -32,8 +32,14 @@ partial class CharacterForm : Form
         tabPage1 = new TabPage();
         tabPage2 = new TabPage();
         panel1 = new Panel();
+        panel2 = new Panel();
+        radioButton1 = new RadioButton();
+        radioButton2 = new RadioButton();
+        radioButton3 = new RadioButton();
+        radioButton4 = new RadioButton();
         SkillTabControl.SuspendLayout();
         tabPage1.SuspendLayout();
+        panel2.SuspendLayout();
         SuspendLayout();
         // 
         // SkillTabControl
@@ -48,6 +54,7 @@ partial class CharacterForm : Form
         // 
         // tabPage1
         // 
+        tabPage1.Controls.Add(panel2);
         tabPage1.Controls.Add(panel1);
         tabPage1.Location = new Point(4, 24);
         tabPage1.Name = "tabPage1";
@@ -74,6 +81,57 @@ partial class CharacterForm : Form
         panel1.Size = new Size(200, 390);
         panel1.TabIndex = 0;
         // 
+        // panel2
+        // 
+        panel2.Controls.Add(radioButton4);
+        panel2.Controls.Add(radioButton3);
+        panel2.Controls.Add(radioButton2);
+        panel2.Controls.Add(radioButton1);
+        panel2.Location = new Point(3, 30);
+        panel2.Name = "panel2";
+        panel2.Size = new Size(57, 46);
+        panel2.TabIndex = 1;
+        // 
+        // radioButton1
+        // 
+        radioButton1.AutoSize = true;
+        radioButton1.Location = new Point(3, 30);
+        radioButton1.Name = "radioButton1";
+        radioButton1.Size = new Size(14, 13);
+        radioButton1.TabIndex = 0;
+        radioButton1.TabStop = true;
+        radioButton1.UseVisualStyleBackColor = true;
+        // 
+        // radioButton2
+        // 
+        radioButton2.AutoSize = true;
+        radioButton2.Location = new Point(15, 30);
+        radioButton2.Name = "radioButton2";
+        radioButton2.Size = new Size(14, 13);
+        radioButton2.TabIndex = 1;
+        radioButton2.TabStop = true;
+        radioButton2.UseVisualStyleBackColor = true;
+        // 
+        // radioButton3
+        // 
+        radioButton3.AutoSize = true;
+        radioButton3.Location = new Point(27, 30);
+        radioButton3.Name = "radioButton3";
+        radioButton3.Size = new Size(14, 13);
+        radioButton3.TabIndex = 2;
+        radioButton3.TabStop = true;
+        radioButton3.UseVisualStyleBackColor = true;
+        // 
+        // radioButton4
+        // 
+        radioButton4.AutoSize = true;
+        radioButton4.Location = new Point(40, 30);
+        radioButton4.Name = "radioButton4";
+        radioButton4.Size = new Size(14, 13);
+        radioButton4.TabIndex = 3;
+        radioButton4.TabStop = true;
+        radioButton4.UseVisualStyleBackColor = true;
+        // 
         // CharacterForm
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
@@ -84,6 +142,8 @@ partial class CharacterForm : Form
         Text = "CharacterForm";
         SkillTabControl.ResumeLayout(false);
         tabPage1.ResumeLayout(false);
+        panel2.ResumeLayout(false);
+        panel2.PerformLayout();
         ResumeLayout(false);
     }
 
@@ -93,4 +153,9 @@ partial class CharacterForm : Form
     private TabPage tabPage1;
     private TabPage tabPage2;
     private Panel panel1;
+    private Panel panel2;
+    private RadioButton radioButton4;
+    private RadioButton radioButton3;
+    private RadioButton radioButton2;
+    private RadioButton radioButton1;
 }
